2625ebe68d40c30a678dce1cf8e0380692a5a70b
Jeremy Mauzy <jeremymauzy@gmail.com>
2015-12-28 08:38:09 -0600

    add views to post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151217162514) do
+ActiveRecord::Schema.define(version: 20151228142837) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -45,12 +45,13 @@ ActiveRecord::Schema.define(version: 20151217162514) do
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
 
   create_table "posts", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.text     "title"
     t.text     "body"
     t.text     "description"
     t.string   "slug"
+    t.integer  "views",       default: 0, null: false
   end
 
   add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true
