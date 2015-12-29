827212ef5786190e08c1035b189f3bb874b8c23e
Jeremy Mauzy <jeremymauzy@gmail.com>
2015-12-29 13:38:51 -0600

    add post editing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,10 @@ class PostsController < ApplicationController
     Post.increment_counter :views, @post.id
   end
   
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -15,6 +19,17 @@ class PostsController < ApplicationController
     end
     redirect_to :back
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:success] = "Post Updated!"
+    else
+      flash[:error] = "Something Went Wrong"
+    end
+    redirect_to @post
+  end
+      
   
   def destroy
     Post.find(params[:id]).destroy
