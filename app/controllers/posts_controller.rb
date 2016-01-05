3cf3bbbc005e9bfe1840341259d43d518baeb3ec
Jeremy Mauzy <jeremymauzy@gmail.com>
2016-01-05 11:23:47 -0600

    add dynamic page titles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @title = @post.title + " | Mauzy.io"
     Post.increment_counter :views, @post.id
   end
   
