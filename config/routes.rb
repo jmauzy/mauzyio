e927891247dd07686632ff78541220dfdfdf0a09
Jeremy Mauzy <jeremymauzy@gmail.com>
2016-01-08 16:29:31 -0600

    standalone contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins
   root 'static_pages#index'
   get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
   #get 'blog/:id' => 'posts#show'
   resources :blog, :as => :posts, :controller => :posts
   get 'blog/' => 'posts#show'
