97cc0a8582d63ca39356b9f614ab8e07b2f9db35
Jeremy Mauzy <jeremymauzy@gmail.com>
2016-06-13 12:42:01 -0500

    Refactor markdown parsing

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,30 +1,28 @@
 module PostsHelper
   class CodeRayify < Redcarpet::Render::HTML
-  def block_code(code, language)
-    CodeRay.scan(code, language).div
+    def block_code(code, language)
+      CodeRay.scan(code, language).div
+    end
   end
-end
 
   def markdown(text)
-    coderayified = CodeRayify.new( 
-                                  :hard_wrap => true)
-  options = {
-    :fenced_code_blocks => true,
-    :no_intra_emphasis => true,
-    :autolink => true,
-    :strikethrough => true,
-    :lax_html_blocks => true,
-    :superscript => true
-
-  }
-  markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
-  markdown_to_html.render(text).html_safe
+    coderayified = CodeRayify.new(:hard_wrap => true)
+    options = {
+      :fenced_code_blocks => true,
+      :no_intra_emphasis => true,
+      :autolink => true,
+      :strikethrough => true,
+      :lax_html_blocks => true,
+      :superscript => true
+    }
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
   end
 
   def popular_posts
-Post.order(views: :desc).limit(5).select([:title, :slug, :views])
+    Post.order(views: :desc).limit(5).select([:title, :slug, :views])
   end
 
 
 
-  end
+end
